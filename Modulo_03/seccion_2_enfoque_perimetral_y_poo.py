b96c77747c077e5c4af2c4d94dd7ee6279072d59
pipedev <fcf.remote@gmail.com>
2025-07-09 12:14:04 -0400

    feat(ejercicios): ejercicios 1 y 2 - laboratorio - módulo 3 - sección 2 - Cisco Python Essentials II

--- Modulo_03/seccion_2_enfoque_perimetral_y_poo.py
+++ Modulo_03/seccion_2_enfoque_perimetral_y_poo.py
@@ -168,3 +168,130 @@ print(adding_stack.get_sum())  # Output: 6
 # Para que el atributo sea considerado privado, debe escribirse con doble guion bajo al inicio.
 # La línea correcta sería: self.__venomous = True
  
+
+# Laboratorio:
+
+# Ejercicio 01:
+
+"""
+    Recientemente te mostramos cómo extender las posibilidades de Stack definiendo una nueva 
+    clase (es decir, una subclase) que retiene todos los rasgos heredados y agrega algunos nuevos.
+
+    Tu tarea es extender el comportamiento de la clase Stack de tal manera que la clase pueda 
+    contar todos los elementos que son agregados (push) y quitados (pop). Emplea la clase Stack 
+    que proporcionamos en el editor.
+
+    Sigue las sugerencias:
+
+    - Introduce una propiedad diseñada para contar las operaciones pop y nombrarla de una manera 
+      que garantice que esté oculta.
+
+    - Inicializala a cero dentro del constructor.
+
+    - Proporciona un método que devuelva el valor asignado actualmente al contador (nómbralo get_counter()).
+      Completa el código en el editor. Ejecútalo para comprobar si tu código da como salida 100.
+"""
+
+# Desarrollo
+
+class Stack:
+    def __init__(self):
+        self.__stk = []
+
+    def push(self, val):
+        self.__stk.append(val)
+
+    def pop(self):
+        val = self.__stk[-1]
+        del self.__stk[-1]
+        return val
+    
+class CountingStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+        self.__counter = 0
+
+    def get_counter(self):
+        return self.__counter
+
+    def pop(self):
+        self.__counter += 1
+        return Stack.pop(self)
+
+stk = CountingStack()
+for i in range(100):
+    stk.push(i)
+    stk.pop()
+print(stk.get_counter())
+
+
+# Ejercicio 02:
+
+"""
+    Como ya sabes, una cola es una estructura de datos que realiza el modelo LIFO (último en entrar, 
+    primero en salir). Es fácil y ya te has acostumbrado a ello perfectamente.
+
+    Probemos algo nuevo ahora. Una cola (queue) es un modelo de datos caracterizado por el término FIFO: 
+    primero en entrar, primero en salir. Nota: una cola (fila) regular que conozcas de las tiendas u oficinas 
+    de correos funciona exactamente de la misma manera: un cliente que llegó primero también es el primero 
+    en ser atendido.
+
+    Tu tarea es implementar la clase Queue con dos operaciones básicas:
+
+    - put(elemento), que coloca un elemento al final de la cola.
+    - get(), que toma un elemento del principio de la cola y lo devuelve como resultado (la cola no 
+      puede estar vacía para realizarlo correctamente).
+    
+    Sigue las sugerencias:
+
+    - Emplea una lista como tu almacenamiento (como lo hicimos con la cola).
+    - put() debe agregar elementos al principio de la lista, mientras que get() debe eliminar los elementos 
+      del final de la lista.
+    - Define una nueva excepción llamada QueueError (elige una excepción de la cual se derivará) y generala 
+      cuando get() intente operar en una lista vacía.
+    
+    Completa el código que te proporcionamos en el editor. Ejecútalo para comprobar si tu salida es similar 
+    a la nuestra.
+"""
+
+# Salida Esperada:
+
+"""
+    1
+    perro
+    False
+    Queue error
+"""
+
+# Desarrollo:
+
+class QueueError(IndexError):  # Eligir la clase base para la nueva excepción.
+        pass
+
+
+class Queue:
+    def __init__(self):
+        self.__cola = []
+
+    def put(self, elem):
+        self.__cola.insert(0, elem)
+
+    def get(self):
+        if len(self.__cola) > 0:
+             elem = self.__cola[-1]
+             del self.__cola[-1]
+             return elem
+        else:
+             raise QueueError
+     
+que = Queue()
+que.put(1)
+que.put("perro")
+que.put(False)
+
+try:
+    for i in range(4):
+        print(que.get())
+except:
+    print("Queue error")
+    
