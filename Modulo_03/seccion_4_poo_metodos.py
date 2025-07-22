da386c94d5fc7fb19cf87244ccfdf8efdc4ff2cc
pipedev <fcf.remote@gmail.com>
2025-07-21 20:16:19 -0400

    feat(ejercicios): ejercicio 2 - laboratorio - módulo 3 - sección 4 - curso Cisco Python Essentials II

--- Modulo_03/seccion_4_poo_metodos.py
+++ Modulo_03/seccion_4_poo_metodos.py
@@ -357,3 +357,72 @@ print(timer)
 timer.prev_second()
 print(timer)
     
+
+# Ejercicio 02 - Días de la semana
+
+"""
+    Tu tarea es implementar una clase llamada Weeker. Sí, tus ojos no te engañan, este nombre 
+    proviene del hecho de que los objetos de esta clase podrán almacenar y manipular los días 
+    de la semana.
+
+    El constructor de la clase acepta un argumento: una cadena. La cadena representa el nombre 
+    del día de la semana y los únicos valores aceptables deben provenir del siguiente conjunto:
+
+    Lun Mar Mie Jue Vie Sab Dom
+
+    Invocar al constructor con un argumento desde fuera de este conjunto debería generar la 
+    excepción WeekDayError (defínela tu mismo; no te preocupes, pronto hablaremos sobre la 
+    naturaleza objetiva de las excepciones). La clase debe proporcionar las siguientes facilidades:
+
+    - Los objetos de la clase deben ser "imprimibles", es decir, deben poder convertirse implícitamente 
+      en cadenas de la misma forma que los argumentos del constructor.
+    - La clase debe estar equipada con métodos de un parámetro llamados add_days(n) y subtract_days(n), 
+      siendo n un número entero que actualiza el día de la semana almacenado dentro del objeto mediante 
+      el número de días indicado, hacia adelante o hacia atrás.
+    - Todas las propiedades del objeto deben ser privadas.
+    
+    Completa la plantilla que te proporcionamos en el editor, ejecuta su código y verifica si tu 
+    salida se ve igual que la nuestra. Completa la plantilla que te proporcionamos en el editor, 
+    ejecuta su código y verifica si tu salida se ve igual que la nuestra.
+"""
+
+# Salida Esperada:
+"""
+    Lun
+    Mar
+    Dom
+    Lo siento, no puedo atender tu solicitud
+"""
+
+class WeekDayError(Exception):
+    pass
+
+class Weeker:
+    __semana = ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom']
+ 
+    def __init__(self, day):
+        try:
+            self.__index_day = Weeker.__semana.index(day)
+        except ValueError:
+            raise WeekDayError("Día de la semana no válido")
+      
+    def __str__(self):
+        return self.__semana[self.__index_day] # Devuelve el día de la semana como cadena
+
+    def add_days(self, n):
+        self.__index_day = (self.__index_day + n) % 7
+
+
+    def subtract_days(self, n):
+        self.__index_day = (self.__index_day - n) % 7
+       
+try:
+    weekday = Weeker('Lun')
+    print(weekday)
+    weekday.add_days(15)
+    print(weekday)
+    weekday.subtract_days(23)
+    print(weekday)
+    weekday = Weeker('Lunes')
+except WeekDayError:
+    print("Lo siento, no puedo atender tu solicitud.")
