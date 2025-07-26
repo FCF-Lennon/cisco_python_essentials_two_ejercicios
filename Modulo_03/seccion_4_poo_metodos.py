65456396e55182af4bc51cebf86a01ec5e651474
pipedev <fcf.remote@gmail.com>
2025-07-26 13:44:02 -0400

    feat(ejercicios): laboratorio - módulo 3 - sección 4 - curso Cisco Python Essentials II - finalizado

--- Modulo_03/seccion_4_poo_metodos.py
+++ Modulo_03/seccion_4_poo_metodos.py
@@ -387,12 +387,15 @@ print(timer)
 """
 
 # Salida Esperada:
+
 """
     Lun
     Mar
     Dom
     Lo siento, no puedo atender tu solicitud
 """
+
+# Desarrollo:
 
 class WeekDayError(Exception):
     pass
@@ -468,6 +471,8 @@ except WeekDayError:
     1.4142135623730951
 """
 
+# Desarrollo:
+
 import math
 
 
@@ -494,3 +499,103 @@ point1 = Point(0, 0)
 point2 = Point(1, 1)
 print(point1.distance_from_point(point2))
 print(point2.distance_from_xy(2, 0))
+
+
+# Ejercicio 04 - Triangulo
+
+"""
+    Ahora vamos a colocar la clase Point (ver Lab anterior) dentro de otra clase. Además, vamos a 
+    poner tres puntos en una clase, lo que nos permitirá definir un triángulo. ¿Cómo podemos hacerlo?
+
+    La nueva clase se llamará Triangle y esto es lo que queremos:
+
+    - El constructor acepta tres argumentos - todos ellos son objetos de la clase Point.
+    - Los puntos se almacenan dentro del objeto como una lista privada.
+    - La clase proporciona un método sin parámetros llamado perimeter(), que calcula el perímetro del 
+      triángulo descrito por los tres puntos; el perímetro es la suma de todas las longitudes de los lados 
+      (lo mencionamos para que conste, aunque estamos seguros de que tú mismo lo conoces perfectamente).
+
+    Completa la plantilla que te proporcionamos en el editor, ejecuta tu código y verifica si tu salida 
+    se ve igual que la nuestra.
+"""
+
+# A continuación puedes copiar el código de la clase Point, el cual se utilizo en el laboratorio anterior:
+
+"""
+    class Point:
+        def __init__(self, x=0.0, y=0.0):
+            self.__x = x
+            self.__y = y
+"""
+
+# Salidad esperada:
+
+""" 
+    3.414213562373095
+"""
+
+# Desarrollo:
+
+import math
+
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.__x = x
+        self.__y = y
+
+class Triangle:
+    def __init__(self, vertice1, vertice2, vertice3):
+       self.__puntos = [vertice1, vertice2, vertice3]
+
+    def perimeter(self):
+        total = 0
+        for i in range(len(self.__puntos)):
+            p1 = self.__puntos[i]
+            p2 = self.__puntos[(i + 1) % len(self.__puntos)]
+            total += math.hypot(p2._Point__x - p1._Point__x, p2._Point__y - p1._Point__y)
+        return total
+
+triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
+print(triangle.perimeter())
+
+
+# o tambien:
+
+"""
+    import math
+
+
+    class Point:
+        def __init__(self, x=0.0, y=0.0):
+            self.__x = x
+            self.__y = y
+
+        def getx(self):
+            return self.__x
+
+        def gety(self):
+            return self.__y
+
+        def distance_from_xy(self, x, y):
+            return math.hypot(abs(self.__x - x), abs(self.__y - y))
+
+        def distance_from_point(self, point):
+            return self.distance_from_xy(point.getx(), point.gety())
+
+
+    class Triangle:
+        def __init__(self, vertice1, vertice2, vertice3):
+            self.__vertices = [vertice1, vertice2, vertice3]
+
+        def perimeter(self):
+            per = 0
+            for i in range(3):
+                per += self.__vertices[i].distance_from_point(self.__vertices[(i + 1) % 3])
+            return per
+
+
+    triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
+    print(triangle.perimeter())
+"""
+
