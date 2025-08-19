f2bf9fff88928b733a312caee6916a7f0c67bb6b
pipedev <fcf.remote@gmail.com>
2025-08-19 13:27:55 -0400

    feat(ejercicios): ejercicio 3 - laboratorio - módulo 4 - sección 3 - curso Cisco Python Essentials II

--- Modulo_04/seccion_3_trabajando_con_archivos_reales.py
+++ Modulo_04/seccion_3_trabajando_con_archivos_reales.py
@@ -399,3 +399,121 @@ finally:
     if output_file:
         output_file.close()
 
+
+# Ejercicio 03 - Evaluando los resultados de los estudiantes
+
+"""
+    El profesor Jekyll dirige clases con estudiantes y regularmente toma 
+    notas en un archivo de texto. Cada línea del archivo contiene 3 
+    elementos: el nombre del alumno, el apellido del alumno y el número 
+    de puntos que el alumno recibió durante ciertas clases.
+
+    Los elementos están separados con espacios en blanco. Cada estudiante 
+    puede aparecer más de una vez dentro del archivo del profesor Jekyll.
+
+    El archivo puede tener el siguiente aspecto:
+
+    John     Smith    5
+    Anna     Boleyn   4.5
+    John     Smith    2
+    Anna     Boleyn   11
+    Andrew   Cox      1.5
+"""
+
+# Tu tarea es escribir un programa que:
+
+"""
+    - Pida al usuario el nombre del archivo del profesor Jekyll.
+    - Lea el contenido del archivo y cuenta la suma de los puntos 
+      recibidos por cada estudiante.
+    - Imprima un informe simple (pero ordenado), como este:
+    
+    Andrew   Cox      1.5
+    Anna     Boleyn   15.5
+    John     Smith    7.0
+"""
+
+# Nota:
+
+"""
+    - Tu programa debe estar completamente protegido contra todas las 
+      fallas posibles: la inexistencia del archivo, el vacío del archivo 
+      o cualquier falla en los datos de entrada; encontrar cualquier error 
+      de datos debería causar la terminación inmediata del programa, y lo 
+      erróneo deberá presentarse al usuario.
+    - Implementa y usa tu propia jerarquía de excepciones: la presentamos 
+      en el editor; la segunda excepción se debe generar cuando se detecta 
+      una línea incorrecta y la tercera cuando el archivo fuente existe 
+      pero está vacío.
+"""
+
+# Consejo: Emplea un diccionario para almacenar los datos de los estudiantes.
+
+
+# Desarrollo:
+
+import errno
+
+class StudentsDataException(Exception):
+    def __init__(self, file, message=''):
+        super().__init__(message)
+        self.file = file
+
+class BadLine(StudentsDataException):
+    def __init__(self, file, line_number, line_content, message=''):
+        super().__init__(file, message)
+        self.line_number = line_number
+        self.line_content = line_content
+
+class FileEmpty(StudentsDataException):
+    def __init__(self, file, message=''):
+        super().__init__(file, message)
+
+
+path_file = './Modulo_04/Archivos/'
+input_extension = '.txt'
+input_file = None
+
+try:
+    file_name = input('Ingrese el nombre del archivo: ').strip().lower()
+    input_file = open(path_file+file_name+input_extension, "r", encoding="utf-8")
+    rows_file = input_file.readlines()
+    point_students = {}
+
+    if len(rows_file) == 0:
+        raise FileEmpty(file_name+input_extension, "El archivo no contiene datos.")
+
+    for n_line, row in enumerate(rows_file, start=1):
+        transform_row = row.split()
+
+        if len(transform_row) != 3:
+            raise BadLine(file_name+input_extension, n_line, row, 'La fila no tiene exactamente tres elementos')
+
+        try:
+            point = float(transform_row[2])
+        except ValueError:
+            raise BadLine(file_name+input_extension, n_line, row, 'El tercer elemento no es un número')
+
+        full_name = f"{transform_row[0]} {transform_row[1]}"
+
+        if full_name not in point_students:
+            point_students[full_name] = 0.0
+
+        point_students[full_name] += point
+
+    for student, points in sorted(point_students.items()):
+        name, surname = student.split()
+        print(f"{name:<10}{surname:<10}{points:.1f}")
+
+except BadLine as e:
+    print(f'Error en {e.file}, línea {e.line_number}: {e}. Contenido: {e.line_content.strip()}')
+except FileEmpty as e:
+    print(f'{e.file}: {e}')
+except IOError as e:
+    print(f'Error: {errno.errorcode[e.errno]}\nDescripción: {e.strerror}\nRuta: {e.filename}')
+except Exception as e:
+    print(f'Error inesperado: {e}')
+finally:
+    if input_file:
+        input_file.close()
+
